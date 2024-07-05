a459385b3d16459ae0619762f02e0f419bb73a3a
Venkata Suresh <shashankyarava1234@gmail.com>
2024-07-05 22:32:31 +0530

    Updated Skills and achievements

--- src/Components/Achievments.js
+++ src/Components/Achievments.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Achievements = () => {
     return (
         <div className='achievements centered-container'>
-            <p className='justify padding'>Finalist in the Bioinformatics Contest 2021, showcasing expertise in the field.</p>
+            <p className='justify padding'>Finalist in the Bioinformatics Contest 2021.</p>
             <p className='justify padding'>CodeChef: Solved 700 problems, demonstrating strong problem-solving skills.</p>
             <p className='justify padding'> LeetCode: Completed 268 problem-solving challenges and earned the 100 Days Badge in 2022.</p>
         </div>
